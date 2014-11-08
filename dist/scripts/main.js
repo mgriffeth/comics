aebfd919c9e5ddf3103577b3e61d1846e03b0de9
Matt Griffeth <mgriffeth9@gmail.com>
2014-11-08 14:02:16 -0500

    deploy

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,82 +1,249 @@
-var Comic = Backbone.Model.extend({
+
+(function(){
+  App.Models.Comic = Backbone.Model.extend({
   defaults:{
     title:'',
+    issue:'',
     publisher:'',
-    hero:''
+    hero:'',
+    description:'',
+    rating:'',
+    image:'http://www.comicbooktidbits.com/Generic%20Site%20Enty_files/image037.jpg'
   },
 
   idAttribute:'_id',
 
   initialize:function(){
-    console.log('Comic instance created.');
+    var c = this.get('title');
+    //console.log(c + " was added.");
   }
 
 });
+}());
 
-var Comics = Backbone.Collection.extend({
-  model: Comic,
+(function(){
+App.Collections.Comics = Backbone.Collection.extend({
+  model: App.Models.Comic,
   url:'http://tiy-atl-fe-server.herokuapp.com/collections/mcgComics'
 });
-
-var ComicsView = Backbone.View.extend({
-
-
-  tagName: 'ul',
-  className: 'comicsList',
-
-  initialize: function(options){
-    console.log('View Initialized');
-    this.render(options.collection);
-  },
-
-  render: function(collection){
-    var template = $('#comic').html();
-    var rendered = _.template(template);
-
-    var self = this;
+}());
 
 
-    _.each(collection.models,function(x){
-    //  console.log(x.get('title'));
-    self.$el.append(rendered(x.attributes));
+//console.log('here');
+(function () {
+//console.log('here2');
+  App.Views.ComicsView = Backbone.View.extend({
 
-    });
-    console.log(this.el);
-    $('#comics').html(this.el);
+    tagName: 'ul',
+    className: 'comicsList',
 
-    return this;
-  }
+    events:{
+      //'click li': 'deleteComic'
+    },
+
+    template: _.template($('#comic').html()),
+
+    initialize: function () {
+
+      this.render();
+
+      this.collection.off();
+      console.log();
+      App.comics_list.on('sync', this.render, this);
+      //App.comics_list.on('destroy', this.render, this);
+
+      $('#comics').html(this.$el);
+
+    },
+
+    render: function () {
+
+      var self = this;
+    //  console.log('here in render');
+       this.$el.empty();
+
+      this.collection.forEach(function (c) {
+        console.log(c);
+        self.$el.append(self.template(c.toJSON()));
+      });
+
+      return this;
+    },
+
+    deleteComic: function (e) {
+      e.preventDefault();
+      console.log('delete '+ e.target.id);
+      var deleteId = $(e.target).attr('id');
+      //console.log(deleteId);
+      var toDelete = App.comics_list.get(deleteId);
+    //  console.log(deleteId + "and again");
+      toDelete.destroy();
+
+
+    }
+  });
+}());
+
+(function(){
+
+App.Views.ComicAdd = Backbone.View.extend({
+    //el: '#comicsForm',
+    events:{
+      'submit #comicInput': 'addComic'
+    },
+
+    initialize: function(){
+      this.render();
+      $('#comics').html(this.$el);
+
+    },
+
+    render: function(){
+    //  console.log('3');
+      var form_html = $('#inputScript').html();
+      this.$el.html(form_html);
+
+    },
+    addComic:function(event){
+      event.preventDefault();
+      title = $('#comicTitle').val();
+      publisher = $('#comicPublisher').val();
+      issue = $('#comicIssue').val();
+      image = $('#comicImage').val();
+
+      comic = new App.Models.Comic({
+        title: title,
+        publisher:publisher,
+        issue:issue,
+        image:image,
+
+      });
+
+      App.comics_list.add(comic).save(null,{
+        success: function(){
+            App.Router.navigate('',{ trigger : true });
+        }
+      })
+
+
+
+
+
+      //$('#comicInput')[0].reset();
+
+
+
+
+    }
 });
 
-console.log('EXCELSIOR!!!');
+}());
 
-var comics_list = new Comics(),
-  comic
-  // title,
-  // publisher,
-  // hero;
+(function(){
 
-  comics_list.fetch().done(function(){
-    console.log(comics_list);
-    new ComicsView({
-      collection: comics_list
-    });
+  App.Views.SingleComic = Backbone.View.extend({
+
+    tagName: 'ul',
+    className: 'singleComic',
+
+    events:{
+      'click #updateBtn' : 'updateComic',
+      'click #deleteBtn': 'deleteComic'
+    },
+    template: _.template($('#singleComic').html()),
+
+    initialize: function(options){
+        this.options=options;
+        this.render();
+
+        $('#comicsForm').empty();
+
+        $('#comics').html(this.$el);
+    },
+    render: function(){
+      console.log('single render');
+
+      this.$el.empty();
+      this.$el.html(this.template(this.options.comic.toJSON()));
+  },
+
+  updateComic: function(e){
+    e.preventDefault();
+
+    this.options.comic.set({
+      title: $('#updateTitle').val(),
+      issue: $('#updateIssue').val(),
+      publisher: $('#updatePublisher').val(),
+      image:$('#updateImage').val(),
+      hero: $('#updateHero').val(),
+      description: $('#updateDescription').val()
+    })
+    this.options.comic.save()
+    App.Router.navigate( " ", {trigger: true});
+
+
+
+
+
+  },
+
+  deleteComic: function(e){
+    e.preventDefault();
+    this.options.comic.destroy()
+
+    App.Router.navigate( '', {trigger: true});
+
+
+    // this.options.comic.save();
+
+  }
+
+
+
   });
 
-  $('#comic-form').submit(function(event){
-    event.preventDefault();
-    title = $('#titleInput').val();
-    publisher = $('#publisherInput').val();
-    hero = $('#heroInput').val();
-    console.log(title + ' , ' + publisher + ' , '+ hero);
+
+}());
+
+(function(){
+    App.Routers.AppRouter = Backbone.Router.extend({
 
 
-    comic = new Comic({
-      title: title,
-      publisher: publisher,
-      hero: hero
+        initialize: function(){
+            Backbone.history.start();
+        },
+        routes:{
+            '':'home',
+            'edit/:id': 'editComic',
+            'add': 'addComic'
+        },
+
+        home: function(){
+            //console.log('heeellloooo');
+          //  new App.Views.ComicAdd();
+            new App.Views.ComicsView({ collection: App.comics_list });
+        },
+        editComic:function(id){
+
+            var c = App.comics_list.get(id);
+            new App.Views.SingleComic({ comic: c });
+
+
+        },
+        addComic: function(){
+          new App.Views.ComicAdd();
+        }
+
     });
+}());
 
-    comics_list.add(comic).save();
-    $(this)[0].reset();
-  })
+//console.log('EXCELSIOR!!!');
+//new App.Views.ComicAdd();
+ App.comics_list = new App.Collections.Comics();
+
+ 
+
+  App.comics_list.fetch().done(function(){
+    //console.log(App.comics_list);
+    App.Router = new App.Routers.AppRouter();
+  });
